4b44ca694d0cc7b0af65e37e231279ea368c37c7
rosendopili <rosendopili@gmail.com>
2020-01-15 15:30:32 -0500

    assignment 2

--- udemy/assignment2/src/App.js
+++ udemy/assignment2/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Validation from './ValidationComponent/Validation';
+import Char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> sanity check </h1> 
-    </div>
-  );
+class App extends Component {
+  state = {
+    userInput: ''
+  }
+  inputChangedHandler = (event) => {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split("");
+    text.splice(index, 1);
+    const updatedText = text.join("");
+    this.setState({
+      userInput: updatedText
+    });
+  }
+
+  render() {
+    const charList = this.state.userInput.split("").map((char, index) =>{
+      return <Char
+            character={char}
+            key={index}
+            clicked={() => this.deleteCharHandler(index)}/>;
+    });
+    return (
+      <div className="App">
+          <input type="text" onChange={this.inputChangedHandler} value={this.state.userInput} />
+            <p>{this.state.userInput}</p>
+            <Validation inputLength={this.state.userInput.length}/>
+            {charList}
+      </div>
+    );
+  }
 }
 
 export default App;
