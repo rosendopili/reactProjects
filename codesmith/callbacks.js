4abf1b99c7e64347a7c6a698e98e81c080ac6f82
rosendopili <rosendopili@gmail.com>
2020-01-14 22:59:18 -0500

    final notes for hardpartsCallbacks and Higher Order functions 1/14/2020

--- codesmith/callbacks.js
+++ codesmith/callbacks.js
@@ -17,9 +17,38 @@ function multiplyBy2(inputNumber){
 //function definition is not analyzed by javascript until the function is invoked in global execution context.
 
 const output = multiplyBy2(num);
-//function level execution context consists of thread of execution and local memory// 
+//function level execution context consists of thread of execution and local memory//
 //once Javascript interprets the parameters set by the function multiplyBy2, the output result is returned, popped off of the call stack and collected in garbage collection.
 //return results are returned to global context.
 const newOutput = multiplyBy2(10);
 
 //being able to pass in a function definition as an argument to the invocation of another function;
+
+function copyArrayAndMultiplyBy2(array) {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] * 2);
+  }
+  return output;
+}
+
+const myArray = [1,2,3];
+const result = copyArrayAndMultiplyBy2(myArray);
+// this returns a value to result and saves the result in Variable Environment
+
+function copyArrayAndManipulate(array, instructions){
+  const output = [];
+  for (let i = 0; i < array.length; i++){
+    output.push(instructions(myArray));
+  }
+  return output;
+}
+
+function multiplyBy2(input){
+  return input * 2;
+}
+
+const result = copyArrayAndManipulate([1,2,3], multiplyBy2);
+//Callback + higher order function utilizing a function within a function.
+
+//Higher order functions are functions that take a function as an argument or return a function.
