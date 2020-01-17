b642c7b8c4ecfbc8a0f0c28ed6ead7ed63e1b1ae
rosendopili <rosendopili@gmail.com>
2020-01-17 17:08:18 -0500

    module 70

--- udemy/react-complete-guide/src/App.js
+++ udemy/react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
 
 class App extends Component {
 state = {
@@ -56,7 +57,11 @@ render() {
     font: 'inherit',
     border: '1px solid black',
     padding: '8px',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: 'lightgreen',
+      color: 'black'
+    }
   };
 
   let persons = null;
@@ -74,12 +79,27 @@ render() {
           })}
         </div>
     );
-    style.backgroundColor = 'red';
+
+    style.backgroundColor = "red";
+    style[':hover'] = {
+      backgroundColor: 'salmon',
+      color: 'black'
+    }
   }
+
+  const classes = [];
+  if (this.state.persons.length <=2){
+    classes.push("red"); //classes = ['red']
+  }
+  if (this.state.persons.length <=1){
+    classes.push("bold"); //classes = ['red', 'bold']
+  }
+
+
     return (
       <div className="App">
         <h1> Hi, I'm a react app</h1>
-        <p> this is really working!</p>
+        <p className={classes.join(" ")}> this is really working!</p>
         <button
           style ={style}
           onClick={this.togglePersonsHandler}> Toggle Persons
