ec949c7a4769b4a6a247ed3a1e5bb8525d07ea46
rosendopili <rosendopili@gmail.com>
2020-01-23 13:09:46 -0500

    movie app with npx install

---
+++ my-movie-app/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Movie from './Movie.js';
+import Search from "./Search.js";
+import './App.css';
+import unirest from 'unirest';
+
+class App extends React.Component {
+
+  state = {
+    movies: []
+  }
+
+  sendRequest = (title) => {
+     const req = unirest("GET", "https://movie-database-imdb-alternative.p.rapidapi.com/");
+     req.query({
+       "page": "1",
+       "r": "json",
+       "s": title
+     });
+     req.headers({
+       "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+       "x-rapidapi-key": "45e2f50e68msh977cdb13550c471p169192jsne02100eccf4c"
+     });
+     req.end((res) => {
+       const movies = res.body.Search;
+       this.setState({movies});
+      });
+  }
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.movies.map((movie) => {
+            return <Movie {...movie}/>
+          })
+        }
+          <Search handleSendRequest={this.sendRequest}/>
+      </div>
+    );
+  }
+}
+
+export default App;
