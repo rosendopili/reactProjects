e47b82873d56117c6649090b23c5ef01fb254ada
rosendopili <rosendopili@gmail.com>
2020-02-14 09:18:37 -0500

    module 147

--- udemy/react-complete-guide/src/components/Persons/Person/Person.js
+++ udemy/react-complete-guide/src/components/Persons/Person/Person.js
@@ -13,8 +13,11 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render(){
@@ -22,9 +25,11 @@ class Person extends Component {
 
     return(
     <Aux>
-      <AuthContext.Consumer>
-        {(context) => context.authenticated ?  <p> Authenticated </p> : <p> Please Log in! </p>}
-      </AuthContext.Consumer>
+      {this.context.authenticated ?  (
+        <p> Authenticated </p>
+      ): (
+        <p> Please Log in! </p>
+      )}
             <p onClick={this.props.click}>
             I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p key="i2">{this.props.children}</p>
