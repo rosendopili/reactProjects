9246f69e7accebd493162defd8d7587f365541d5
rosendopili <rosendopili@gmail.com>
2020-02-26 14:40:56 -0500

    module 172

--- udemy/burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ udemy/burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false
   }
 
   updatePurchaseState (ingredients) {
@@ -68,6 +69,18 @@ class BurgerBuilder extends Component {
 
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You continue!');
+  }
+
   render() {
 
     const disabledInfo = {
@@ -78,8 +91,11 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
-          <OrderSummary ingredients={this.state.ingredients}/>
+        <Modal
+        show={this.state.purchasing}
+        modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary ingredients={this.state.ingredients}
+          purchaseCancelled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler}/>
         </Modal>
 
         <Burger ingredients={this.state.ingredients}/>
@@ -88,7 +104,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved = {this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
-          price={this.state.totalPrice} />
+          price={this.state.totalPrice}
+          ordered={this.purchaseHandler} />
       </Aux>
 
     );
